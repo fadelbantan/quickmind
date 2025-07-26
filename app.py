1d3304946225eda9db91e6fb1cab16a2ee0c08e2
Fadil Bantan <fadooly12@gmail.com>
2025-07-26 05:25:29 +0300

    first main node added

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
