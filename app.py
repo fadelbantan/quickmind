0fe480deec40228a4cf78bc4ddecf885ba306071
Fadil Bantan <fadooly12@gmail.com>
2025-07-27 17:33:49 +0300

    zoomable screen

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    zoom_controls = """
+    <div id='controls'>
+        <button id='zoom-in' title='Zoom In'>+</button>
+        <button id='zoom-out' title='Zoom Out'>-</button>
+    </div>
+    """
+    return render_template("index.html", zoom_controls=zoom_controls)
+
+
 
 app.run(debug=True)
