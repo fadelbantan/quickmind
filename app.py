4df378f0a44264445a094484dfc64f2aea1a38cd
Fadil Bantan <fadooly12@gmail.com>
2025-07-27 18:46:13 +0300

    better zoom interface

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     zoom_controls = """
-    <div id='controls'>
-        <button id='zoom-in' title='Zoom In'>+</button>
+    <div id='controls'>        
         <button id='zoom-out' title='Zoom Out'>-</button>
+        <span id='zoom-display'>100%</span>
+        <button id='zoom-in' title='Zoom In'>+</button>
     </div>
     """
     return render_template("index.html", zoom_controls=zoom_controls)
